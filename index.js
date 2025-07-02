8e73baaced17ddfe3bfae9792659d1f5ce292291
Kodjo Egloh <kodjoegloh@R2Hs-MacBook-Pro.local>
2025-07-02 15:02:28 -0400

    second commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// function first(){
+//     let box1 = document.getElementById("box2")
+//     let newe = document.createElement('section')
+//     document.write(<h1 class = "fw-bolder text-center text-danger">hello</h1>
+
+//     )
+    
+
+
+// }
+
+document.write(<h1 class = "text-danger">hello</h1> )
